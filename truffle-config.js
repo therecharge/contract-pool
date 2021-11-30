822485dd0ef1ed6a20db3d2bfa08d97034be7269
humblefirm <humblefirm@naver.com>
2021-11-30 14:34:12 +0900

    setting

--- truffle-config.js
+++ truffle-config.js
@@ -11,8 +11,7 @@ module.exports = {
     },
     testnet: {
       provider: () =>
-        new HDWalletProvider(
-          mnemonic,
+      new HDWalletProvider([mnemonic],
           `https://data-seed-prebsc-1-s1.binance.org:8545`
         ),
       network_id: 97,
