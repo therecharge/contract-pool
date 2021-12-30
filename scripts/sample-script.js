73a8ca49a38cbf993554f60f248635ecc3ccc68e
moon <breaklee@naver.com>
2021-12-30 17:27:26 +0900

    fix

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -15,12 +15,22 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  // const Greeter = await hre.ethers.getContractFactory("Greeter");
-  // const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const PoolV2 = await hre.ethers.getContractFactory("PoolV2");
+  // const poolV2 = await PoolV2.deploy(
+  //   "1.3 BSC Locked Pool 100", 
+  //   true, 
+  //   false, 
+  //   1641186000,
+  //   1 * 60 * 60 * 24 * 28,
+  //   0,
+  //   "0x2D94172436D869c1e3c094BeaD272508faB0d9E3", 
+  //   "0x2D94172436D869c1e3c094BeaD272508faB0d9E3");
+  const pool = await PoolV2.at(
+    "0xA7ad096Ca2364EaE97F96e836f08dbb2b9b0b70B"
+  );
 
-  // await greeter.deployed();
-
-  // console.log("Greeter deployed to:", greeter.address);
+  await pool.setRewardDistribution("0x82C02b9E84eeF14354698AD48dc99Caf5261C568");
+  console.log("PoolV2 deployed to:", PoolV2.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
